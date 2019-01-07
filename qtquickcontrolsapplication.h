bdec0443898bca054882c91e763efb4fd07e7019
pzhao004 <lanquanyouyu@163.com>
2019-01-07 18:34:37 +0800

    Create qtquickcontrolsapplication.h

---
+++ qtquickcontrolsapplication.h
@@ -0,0 +1,11 @@
+#ifndef QTQUICKCONTROLSAPPLICATION_H
+#define QTQUICKCONTROLSAPPLICATION_H
+
+
+#include <QtGui/QGuiApplication>
+#include <QDebug>
+QT_BEGIN_NAMESPACE
+#define QtQuickControlsApplication QGuiApplication
+QT_END_NAMESPACE
+
+#endif // QTQUICKCONTROLSAPPLICATION_H
